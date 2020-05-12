9c08d41e7dafd876e4a80f471aa5a038af1de7ec
Ritesh Sharma <sharmaritesh3312@gmail.com>
2020-05-12 22:09:41 +0530

    iteratives video done.

---
+++ iteratives.dart
@@ -0,0 +1,29 @@
+
+void main() {
+
+  int n = 10;
+
+  // for loop in dart
+  // for (var i = 0; i <= n; i++) {
+  //   if (i % 2 == 0) {
+  //     print(i);
+  //   }
+  // }
+
+  // While loop
+  // int i = 0;
+  // while (i <= n) {
+  //   if (i % 2 == 0) {
+  //     print(i);
+  //   }
+  //   i++;
+  // }
+
+  // for-in loop
+  List arr = [1, 2, 3, 4, 5, 6];
+  
+  for (var element in arr) {
+    print(element * 2);
+  }
+
+}
